7f491c6a19140635cfde34da95cc33b15ab33868
TzvetanIG <cecoig@mail.bg>
2015-05-15 18:10:22 +0300

    added model Language

---
+++ Data/Repositories/LanguagesRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Data.Repositories
+{
+    using System.Data.Entity;
+    using Models;
+
+    public class  LanguagesRepository : GenericRepository<Language>
+    {
+        public LanguagesRepository(DbContext context)
+            : base(context)
+        {
+        }
+    }
+}
