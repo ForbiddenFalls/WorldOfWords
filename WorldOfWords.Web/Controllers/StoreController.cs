05b945e1c220be65bd6544101213d4a5756769bf
Ivelin Kirilov <ivelinkv@gmail.com>
2015-05-17 17:01:21 +0300

    List words in the store with paging and search

--- WorldOfWords.Web/Controllers/StoreController.cs
+++ WorldOfWords.Web/Controllers/StoreController.cs
@@ -3,16 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 
 namespace WorldOfWords.Web.Controllers
 {
     [Authorize]
-    public class StoreController : Controller
+    public class StoreController : BaseController
     {
-        // GET: Store
-        public ActionResult Index()
+        private const int PageSize = 3;
+
+        public ActionResult Index(string searchString, string currentFilter, int? page)
         {
-            return View();
+            var words = this.Data.Words.OrderBy(w => w.Content).AsQueryable();
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+            int pageNumber = (page ?? 1);
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                words = words.Where(w => w.Content.Contains(searchString));
+            }
+
+            return View(words.ToPagedList(pageNumber, PageSize));
         }
     }
 }
