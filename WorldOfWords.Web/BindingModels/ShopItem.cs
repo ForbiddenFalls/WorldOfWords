7816e6cd4fb7bb6ddca11409ccb32b38a7ec99dc
Ivelin Kirilov <ivelinkv@gmail.com>
2015-05-23 11:34:02 +0300

    Upgrades to store (purchase by quantity, have a shop cart). Unfinished.

---
+++ WorldOfWords.Web/BindingModels/ShopItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WorldOfWords.Web.BindingModels
+{
+    public class ShopItem
+    {
+        public int WordId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
