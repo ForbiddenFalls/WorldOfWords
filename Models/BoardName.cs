3d6d25c1a8d732ddb15054df868320b3fff12597
TzvetanIG <cecoig@mail.bg>
2015-05-27 17:49:27 +0300

    added BoardName.cs

---
+++ Models/BoardName.cs
@@ -0,0 +1,22 @@
+﻿namespace Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class BoardName
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        public int LanguageId { get; set; }
+
+        public virtual Language Language { get; set; }
+
+        public int? BoardId { get; set; }
+
+        public virtual Board Board { get; set; }
+    }
+}
